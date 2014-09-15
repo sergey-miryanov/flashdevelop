1b13d99da20fcc979b6f2f35e409dcd35393bae6
slavara <ratdg@ya.ru>
2014-09-15 20:05:46 +0400

    Add AS3TestProj

--- tests/External/Plugins/CodeRefactorTest/Provider/RefactoringHelperTest.cs
+++ tests/External/Plugins/CodeRefactorTest/Provider/RefactoringHelperTest.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluginCore;
 
 namespace CodeRefactorTest.Provider
 {
     [TestClass]
     public class RefactoringHelperTest
     {
+        private static IMainForm mainForm;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
     }
 }
---
+++ tests/External/Plugins/CodeRefactorTest/Resources/AS3TestProj/src/Main.as
@@ -0,0 +1,13 @@
+package {
+	import flash.display.Sprite;
+	
+	/**
+	 * @author SlavaRa
+	 */
+	public class Main extends Sprite {
+		
+		public function Main() {
+			super();
+		}
+	}
+}
