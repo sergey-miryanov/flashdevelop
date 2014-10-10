be34c425e28f39416e18d13fc56254b20bbda782
slavara <ratdg@ya.ru>
2014-10-10 17:35:53 +0400

    create PluginCore.Test

---
+++ tests/PluginCore.Test/PluginCore.Test/PluginCore/Managers/TraceManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluginCore.Managers;
+
+namespace PluginCore.Test.PluginCore.Managers
+{
+    public class TraceManagerTest
+    {
+    }
+
+    [TestClass]
+    public class TraceItemTest
+    {
+        [TestMethod]
+        public void CreateTraceItemWithNullString()
+        {
+            TraceItem item = new TraceItem(null, 0);
+            Assert.AreEqual(null, item.Message);
+            Assert.AreEqual(0, item.State);
+        }
+
+        [TestMethod]
+        public void CreateTraceItemWithEmptyString()
+        {
+            TraceItem item = new TraceItem(string.Empty, 0);
+            Assert.AreEqual(string.Empty, item.Message);
+            Assert.AreEqual(0, item.State);
+        }
+
+        [TestMethod]
+        public void CreateTraceItemWithSomeString()
+        {
+            TraceItem item = new TraceItem("some", 0);
+            Assert.AreEqual("some", item.Message);
+            Assert.AreEqual(0, item.State);
+        }
+    }
+}
