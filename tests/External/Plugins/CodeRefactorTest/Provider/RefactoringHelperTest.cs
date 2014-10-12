f2819075fd39230c7e56fe130040fdb70170091a
slavara <ratdg@ya.ru>
2014-10-12 21:30:30 +0400

    Small refactoring...

--- tests/External/Plugins/CodeRefactorTest/Provider/RefactoringHelperTest.cs
+++ tests/External/Plugins/CodeRefactorTest/Provider/RefactoringHelperTest.cs
@@ -1,25 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PluginCore;
-using PluginCore.Utilities;
-using FlashDevelop.Mock;
 
 namespace CodeRefactor.Test.Provider
 {
     [TestClass]
     public class RefactoringHelperTest
     {
-        [TestInitialize]
-        public void Initialize()
-        {
-            MainForm mainForm = new MainForm();
-            SingleInstanceApp.Initialize();
-            PluginBase.MainForm.OpenEditableDocument("../../Resources/AS3TestProj/src/Main.as");
-        }
-
-        [TestMethod]
-        public void EmptyTest()
-        {
-            Assert.IsNotNull(PluginBase.MainForm);
-        }
     }
 }
