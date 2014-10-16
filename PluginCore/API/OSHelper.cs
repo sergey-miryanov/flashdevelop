b004b05a68b62a6b768d7061a5529a0d775fb15d
slavara <ratdg@ya.ru>
2014-10-16 13:47:10 +0400

    remove extra code

--- PluginCore/API/OSHelper.cs
+++ PluginCore/API/OSHelper.cs
@@ -6,68 +6,26 @@ namespace PluginCore
 {
 	public static class OSHelper
 	{
+		public static readonly APIUtils API;
+
 		static OSHelper()
 		{
-			if(Path.DirectorySeparatorChar == '\\')
-				_api = new WinAPI();
-			else if(IsRunningOnMac())
-				_api = new MacAPI();
-			else if(IsRunningOnLinux())
-				_api = new LinuxAPI();
-			else
+			switch (Environment.OSVersion.Platform)
 			{
-				throw new UnknownOSTypeException(Environment.OSVersion);
+				case PlatformID.Win32NT:
+				case PlatformID.Win32S:
+				case PlatformID.Win32Windows:
+				case PlatformID.WinCE:
+					API = new WinAPI();
+					break;
+				case PlatformID.MacOSX:
+					API = new MacAPI();
+					break;
+				case PlatformID.Unix:
+					API = new LinuxAPI();
+					break;
+				default: throw new Exception(Environment.OSVersion.ToString());
 			}
-		}
-
-		public static APIUtils API
-		{
-			get { return _api; }
-		}
-
-		[DllImport("libc")]
-		static extern int uname(IntPtr buf);
-
-		public static bool IsRunningOnMac()
-		{
-			IntPtr buf = IntPtr.Zero;
-			try
-			{
-				buf = Marshal.AllocHGlobal(8192);
-				if(uname(buf) == 0)
-				{
-					string os = Marshal.PtrToStringAnsi(buf);
-					if(os == "Darwin")
-						return true;
-				}
-			}
-			catch
-			{
-				// don't do anything
-			}
-			finally
-			{
-				if(buf != IntPtr.Zero)
-					Marshal.FreeHGlobal(buf);
-			}
-
-			return false;
-		}
-
-		public static bool IsRunningOnLinux() 
-		{
-			var p = (int) Environment.OSVersion.Platform;
-			return (p == 4) || (p == 6) || (p == 128);
-		}
-
-		private static APIUtils _api;
-	}
-
-	public class UnknownOSTypeException : Exception
-	{
-		public UnknownOSTypeException(OperatingSystem os)
-			: base(os.ToString())
-		{
 		}
 	}
 }
